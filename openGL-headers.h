f005f7a8b5e849adfe1d088404d77223f6cfbccb
Sarah Kurdoghlian <skurdogh@calpoly.edu>
2023-01-29 11:40:39 -0800

    first commit

---
+++ openGL-headers.h
@@ -0,0 +1,16 @@
+#if defined(WIN32)  || defined(_WIN32) 
+#include <Windows.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/freeglut.h>
+#elif defined(linux) || defined(__linux__)
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#elif defined(__APPLE__)
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#endif
+
